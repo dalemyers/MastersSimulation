6c45b096ba1a844db721726f80c9db4efd85dedc
Vel0x <DaleMyers19@gmail.com>
2013-12-20 16:08:14 +0000

    Got rid of IDE files and just left code

--- test/core/lib/testutil.cc
+++
@@ -1,9 +0,0 @@
-#include "testutil.h"
-
-void printEnum(const char *enumname)
-{
-    cEnum *e = cEnum::find(enumname);
-    ASSERT(e!=NULL);
-    ASSERT(strcmp(e->getFullName(), enumname)==0);
-    ev << e->getFullName() << ": " << e->info() << "\n";
-}
--- test/core/lib/testutil.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTUTIL_H
-#define TESTUTIL_H
-
-#include <omnetpp.h>
-
-void printEnum(const char *enumname);
-
-#endif
